17ff075ce53c56fd196bd5196888df43b12666d2
Hiroshi Saito <hiroshi3110@gmail.com>
2009-02-07 23:50:41 +0900

    update rails version from 2.2.2 to 2.3.0 (RC1)

--- config/initializers/modules/try.rb
+++ config/initializers/modules/try.rb
@@ -1,8 +1,9 @@
-class Object
-  def try(*methods)
-    #self.nil? ? nil : self.send(method)
-    methods.inject(self) do |obj, method|
-     obj.nil? ? nil : obj.send(method) 
-    end
-  end
-end
+# This is deprecated. Instead of this use activesupport-2.3.0/lib/active_support/core_ext/try.rb
+# class Object
+#   def try(*methods)
+#     #self.nil? ? nil : self.send(method)
+#     methods.inject(self) do |obj, method|
+#      obj.nil? ? nil : obj.send(method) 
+#     end
+#   end
+# end
---
+++ test/unit/helpers/application_helper_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test "auto_complete_combo_box_tag" do
+    # @controller = TestController.new
+#    p params
+#    auto_complete_combo_box_tag "foo", ["bar", "bazz"]
+  end
+end
