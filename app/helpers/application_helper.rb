1e9daebb2d4ab0702a0c06f47598f9d8e1baac02
Hiroshi Saito <hiroshi3110@gmail.com>
2009-02-08 11:27:51 +0900

    add user_name search combo box, etc

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
     (completion_options[:skip_style] ? "" : auto_complete_stylesheet) +
     text_field_tag(name, value, tag_options) +
     content_tag("div", "", :id => "#{name}_auto_complete", :class => "auto_complete") +
-    javascript_tag("var #{name}_auto_completer = new Autocompleter.LocalCombo('#{name}', '#{name}_auto_complete', #{array}, {frequency:0.1, partialSearch:true, fullSearch:true});") +
+    javascript_tag("var #{name}_auto_completer = new Autocompleter.LocalCombo('#{name}', '#{name}_auto_complete', #{array}, {frequency:0.1, partialSearch:true, fullSearch:true, partialChars:1});") +
     link_to_function("▼", "#{name}_auto_completer.toggle();", :style => "text-decoration:none; border:thin solid gray")
     # TODO: to be more pretty button (i.e. use image)
   end
--- app/models/asset.rb
+++ app/models/asset.rb
@@ -13,7 +13,14 @@ class Asset < ActiveRecord::Base
 
   # scopes
   named_scope :user_id, lambda{|user_id|
+    # NOTE: If you use join, old utilizations belongs to assets can satisfy given condition, 
+    # NOTE: I don't have any resolution other than using subquery yet.
+    # {:joins => :utilization, :conditions => ["utilizations.user_id = ?", user_id]}
     {:conditions => ["(SELECT user_id FROM utilizations WHERE asset_id = assets.id ORDER BY started_on DESC LIMIT 1) = ?", user_id]}
+  }
+  named_scope :user_login, lambda{|user_login|
+    # {:joins => {:utilization => :user}, :conditions => ["users.login = ?", user_login]}
+    {:conditions => ["(SELECT users.login FROM utilizations LEFT JOIN users ON users.id = utilizations.user_id WHERE asset_id = assets.id ORDER BY started_on DESC LIMIT 1) %% ?", user_login]}
   }
   named_scope :developer_name, lambda{|developer_name|
     {:joins => :developer, :conditions => ["developers.name %% ?", developer_name]}
